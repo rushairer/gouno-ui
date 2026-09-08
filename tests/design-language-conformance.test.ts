f39a34eca863055a8754f98d276c6bfaede6e645
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-08 18:37:04 +0800

    test: include Blog notifications in design conformance

--- tests/design-language-conformance.test.ts
+++ tests/design-language-conformance.test.ts
@@ -22,6 +22,7 @@ const blogAdminApplicationFiles = [
   resolve(productsRoot, "blog-admin-tags.tsx"),
   resolve(productsRoot, "blog-admin-pages.tsx"),
   resolve(productsRoot, "blog-admin-comments.tsx"),
+  resolve(productsRoot, "blog-admin-notifications.tsx"),
   resolve(productsRoot, "blog-admin-users.tsx"),
   resolve(productsRoot, "blog-admin-site-settings.tsx"),
 ];
