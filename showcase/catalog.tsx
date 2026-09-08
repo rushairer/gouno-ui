5e3cc5dbb1e2ba08e9840fc5566b60f601ffd4f7
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-08 18:12:54 +0800

    feat: add Blog Admin tags navigation

--- showcase/catalog.tsx
+++ showcase/catalog.tsx
@@ -225,6 +225,7 @@ export const showcaseCatalog: ShowcaseGroup[] = [
     item("blog-admin-posts", "Posts", "文章管理", 100, <FileText />),
     item("blog-admin-categories", "Categories", "分类管理", 100, <ListTree />),
     item("blog-admin-comments", "Comments", "评论审核", 100, <MessageSquare />),
+    item("blog-admin-tags", "Tags", "标签管理", 100, <Tags />),
   ]},
   { workspace: "blog-admin", group: "Site 站点管理", items: [
     item("blog-admin-users", "Members", "成员与权限", 100, <CircleUserRound />),
