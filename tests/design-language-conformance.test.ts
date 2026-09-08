a70ca09cfbcfb2eb2c0cdc00f2bacb3cbeb13159
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-08 18:12:39 +0800

    test: include Blog tags in design conformance

--- tests/design-language-conformance.test.ts
+++ tests/design-language-conformance.test.ts
@@ -19,6 +19,7 @@ const gossoApplicationFiles = [
 const blogAdminApplicationFiles = [
   resolve(productsRoot, "blog-admin-posts.tsx"),
   resolve(productsRoot, "blog-admin-categories.tsx"),
+  resolve(productsRoot, "blog-admin-tags.tsx"),
   resolve(productsRoot, "blog-admin-comments.tsx"),
   resolve(productsRoot, "blog-admin-users.tsx"),
   resolve(productsRoot, "blog-admin-site-settings.tsx"),
