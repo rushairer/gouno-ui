96458fb476f44b9ceaf1df270439b13179460c9e
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-18 14:56:58 +0800

    feat(typography): export semantic heading variants

--- src/core/index.ts
+++ src/core/index.ts
@@ -9,7 +9,7 @@ export { Spinner, type SpinnerProps } from "./spinner";
 export { Progress, type ProgressProps } from "./progress";
 export { AspectRatio, type AspectRatioProps } from "./aspect-ratio";
 export { Kbd, type KbdProps } from "./kbd";
-export { Heading, Text, Typography, type HeadingProps, type TextProps, type TypographyProps, type HeadingLevel, type TextSize, type TextTone } from "./typography";
+export { Heading, Text, Typography, type HeadingProps, type TextProps, type TypographyProps, type HeadingLevel, type HeadingVariant, type TextSize, type TextTone } from "./typography";
 export { CodeBlock, type CodeBlockProps } from "./code-block";
 export { Input, SearchField, type InputProps } from "./input";
 export { Textarea, type TextareaProps } from "./textarea";
